1f7f496aca85b5461c6f096997cb720df30ca027
Ruben <ruben-f@outlook.com>
2024-08-15 20:03:38 +0200

    0.0.3

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 2 {
+    } else {
+        panic!("Not enough arguments were provided!");
+    }
+}
