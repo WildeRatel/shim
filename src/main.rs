7e9e2ae96451346cb2957efe3526b1d5f8ff681d
Ruben <ruben-f@outlook.com>
2024-08-18 18:56:34 +0200

    0.0.3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() == 2 {
-        println!("Arg 1: {}", args[1]);
+    if args.len() > 2 {
+        panic!("Too many arguments were supplied!");
     } else {
-        if args.len() > 2 {
-            panic!("Too many arguments were supplied!");
-        } else {
-            panic!("Not enough arguments were supplied!");
-        }
+        panic!("Not enough arguments were supplied!");
     }
 }
