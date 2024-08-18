0c4a3b04ecc9578c57555948c65873103fa9849a
Ruben <ruben-f@outlook.com>
2024-08-18 19:02:49 +0200

    0.0.4

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ fn main() {
     if args.len() > 2 {
         panic!("Too many arguments were supplied!");
     } else {
-        panic!("Not enough arguments were supplied!");
+        if args.len() != 2 {
+            panic!("Not enough arguments were supplied!");
+        }
+    }
+
+    if args[1][args[1].len() - 5..args[1].len()].contains(".txt") {
+        println!("Check pass!");
+    } else {
+        println!("Check fail!");
     }
 }
